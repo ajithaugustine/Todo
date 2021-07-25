77ff363ef728d3e577c79a705c6e8a3e996b94d8
ajithaugustine <ajithaugustine01@gmail.com>
2021-07-25 17:34:22 +0530

    todo2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import {useState} from 'react'
 import './App.css';
+import Popup from './Components/Popup/Popup';
 
 function App() {
   const [list, setlist] = useState('')
   const [store, setstore] = useState([])
+  const [popup, setpopup] = useState(false)
+  const [data, setdata] = useState([])
+  const [index, setindex] = useState()
   const submitdata= (event)=>{
     event.preventDefault()
     setstore([...store,list]) 
@@ -13,8 +17,23 @@ function App() {
        const data = store.filter((value,index)=>index !==key)
        setstore(data)  
   }
+  const itemedit =(value,index)=>{
+    setindex(index)
+    setdata(value)
+    setpopup(true)
+
+  }
+  const confirmedit=(value)=>{
+     store.splice(index,1,value)
+     setpopup(false)
+  }
+  const canceledit=()=>{
+    setpopup(false)
+  }
 
 return(
+  <div>
+
   <div className="container">
      <div className="head">
 
@@ -26,13 +45,13 @@ return(
      <ul>
        {store.map((item,index)=>{
          return(
-
-           <li key={index}><span>{item}</span><i className="fas fa-trash" onClick={()=>itemdelete(index)}></i></li>
-         )
-       })}
-    
- 
+           
+           <li key={index}><span>{item}</span><i className="fas fa-edit"  onClick={()=>itemedit(item,index)}></i><i className="fas fa-trash" onClick={()=>itemdelete(index)}></i></li>
+           )
+          })}
      </ul>
+   </div>
+   { popup? <Popup  itemdata={data} canceledit={()=>canceledit()} confirmedit={(datanew)=>confirmedit(datanew)}/>:''}
    </div>
  )
 }
